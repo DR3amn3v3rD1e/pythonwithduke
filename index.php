ae98e1cf0ebe1b6a5b64646adac1558af9c34af0
trugduke31 <54620006+DR3amn3v3rD1e@users.noreply.github.com>
2023-08-21 15:32:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,80 +1,155 @@
-<?php
-    // error_reporting(0);
-
-    // Create folder for each user
-    session_start();
-    $dir = 'upload/' . session_id();
-    if ( !file_exists($dir) )
-        mkdir($dir);
-
-    if(isset($_GET["debug"])) die(highlight_file(__FILE__));
-    if(isset($_FILES["file"])) {
-        $error = '';
-        $success = '';
-        try {
-            $file = $dir . "/" . $_FILES["file"]["name"];
-            move_uploaded_file($_FILES["file"]["tmp_name"], $file);
-            $success = 'Successfully uploaded file at: <a href="/' . $file . '">/' . $file . ' </a><br>';
-            $success .= 'View all uploaded file at: <a href="/' . $dir . '/">/' . $dir . ' </a>';
-        } catch(Exception $e) {
-            $error = $e->getMessage();
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Free Icon</title>
 
-        <title>PHP upload Level 1</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/bootstrap4-neon-glow.min.css">
+    
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel='stylesheet' href='//cdn.jsdelivr.net/font-hack/2.020/css/hack.min.css'>
+    
+  </head>
+  <body>
 
-        <!-- This is for UI only -->
-        <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    </head>
-    <body>
-        <br/>
-        <br/>
-        <h3 class="display-4 text-center">File upload workshop</h3>
-        <h4 class="display-4 text-center">Level 1</h4>
-        <p class="display-5 text-center">Goal: RCE me!</p>
+  
 
-        <br/>
-        <div class="container">
-            <a href="/?debug">Debug source</a>
-            <form method="post" enctype="multipart/form-data">
-                Select file to upload:
-                <input type="file" name="file" id="file">
-                <br/>
-                <input type="submit">
-            </form>
-            <span style="color:red"><?php echo $error; ?></span>
-            <span style="color:green"><?php echo $success; ?></span>
+<div class="jumbotron bg-transparent mb-0 radius-0">
+  <div class="container">
+      <div class="row">
+        <div class="col-xl-6">
+          <h1 class="display-2">Free Ico<span class="vim-caret">n</span></h1>
+          <div class="lead mb-3 text-mono text-success">FREE icons and stickers for your projects.</div>
+
+          
         </div>
+      </div>
+  </div>
+</div>
 
-    </body>
 
-    <footer class="container">
-        <br/>
-        <br/>
-        <br/>
-        <button class="float-right btn btn-dark" type="button" onclick="nextLevel()">Next level</button>
+<div class="container py-5">
+  <div class="row">
+    <div class="col-lg-3">
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=flag.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Flag</h4>
+            <a href="/loadImage.php?file_name=flag.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
 
-        <script>
-            function prevLevel() {
-                const url = new URL(origin);
-                url.port = (parseInt(url.port) - 1).toString();
-                location.href = url.toString();
-            }
-            function nextLevel() {
-                const url = new URL(origin);
-                url.port = (parseInt(url.port) + 1).toString();
-                location.href = url.toString();
-            }
-        </script>
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=apartment.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Apartment</h4>
+            <a href="/loadImage.php?file_name=apartment.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
+      
+    </div>
+    <div class="col-lg-3">
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=bomb.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Bomb</h4>
+            <a href="/loadImage.php?file_name=bomb.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
 
-    </footer>
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=facebook.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Facebook</h4>
+            <a href="/loadImage.php?file_name=facebook.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
+      
+    </div>
+    <div class="col-lg-3">
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=pizza.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Pizza</h4>
+            <a href="/loadImage.php?file_name=pizza.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
+
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=fruit-shop.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Shop</h4>
+            <a href="/loadImage.php?file_name=fruit-shop.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
+      
+    </div>
+    <div class="col-lg-3">
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=sandwich.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Sandwich</h4>
+            <a href="/loadImage.php?file_name=sandwich.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
+
+      <div class="ht-tm-codeblock">
+        <div class="ht-tm-element card bg-primary text-white mb-3 text-center">
+          <img class="card-img-top" src="/loadImage.php?file_name=money-bag.png" alt="Card image cap" style= 'background-color:white'>
+          <div class="card-body">
+            <h4 class="card-title">Money Bag</h4>
+            <a href="/loadImage.php?file_name=money-bag.png" class="btn btn-light btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left">View</a>
+          </div>
+        </div>
+        
+      </div>
+      
+    </div>
+  </div>
+  
+
+</div>
+
+  
+
+
+  
+</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </body>
 </html>
